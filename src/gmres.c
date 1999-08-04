dd6c480ad2428563dfb7332761ef9209dd35a333
kengo ichiki <kengoichiki@gmail.com>
1999-08-04 19:07:02 +0000

    style change.

--- src/gmres.c
+++ src/gmres.c
@@ -1,6 +1,6 @@
 /* generalized minimum residual method
  * Copyright (C) 1998,1999 Kengo ICHIKI <kengo@caltech.edu>
- * $Id: gmres.c,v 1.6 1999/06/11 05:43:10 ichiki Exp $
+ * $Id: gmres.c,v 1.7 1999/08/04 19:07:02 ichiki Exp $
  *
  * Reference :
  *   GMRES(m) : Y.Saad & M.H.Schultz, SIAM J.Sci.Stat.Comput.
@@ -49,13 +49,13 @@ mygmres_m (int n, double *f, double *x,
   myatimes (n, x, tmp);
   for(i=0; i<n; i++)
     {
-      tmp[i] = f[i] - tmp[i]; /* r0 */
+      tmp [i] = f [i] - tmp [i]; /* r0 */
     }
   /* compute v1 */
-  g[0] = norm (n, tmp); /* beta */
+  g [0] = norm (n, tmp); /* beta */
   for (i=0; i<n; i++)
     {
-      v[0 * n + i] = tmp[i] / g[0];
+      v [0 * n + i] = tmp [i] / g [0];
     }
   /* main loop */
   while (*iter <= itmax)
@@ -65,11 +65,11 @@ mygmres_m (int n, double *f, double *x,
       for (j=0; j<m; j++)
 	{
 	  /* tmp = A.vj */
-	  myatimes (n, &v[j * n], tmp);
+	  myatimes (n, &v [j * n], tmp);
 	  /* h_i,j (i=1,...,j) */
 	  for (i=0; i<=j; i++)
 	    {
-	      h[i * m + j] = inner (n, tmp, &v[i * n]);
+	      h [i * m + j] = inner (n, tmp, &v [i * n]);
 	    }
 	  /* vv_j+1 */
 	  for (k=0; k<n; k++)
@@ -77,48 +77,49 @@ mygmres_m (int n, double *f, double *x,
 	      hv = 0.0;
 	      for (i=0; i<=j; i++)
 		{
-		  hv += h[i * m + j] * v[i * n + k];
+		  hv += h [i * m + j] * v [i * n + k];
 		}
-	      tmp[k] -= hv; /* vv_j+1 */
+	      tmp [k] -= hv; /* vv_j+1 */
 	    }
 	  /* h_j+1,j */
 	  hh = norm (n, tmp); /* h_j+1,j */
 	  /* v_j+1 */
 	  for (k=0; k<n; k++)
 	    {
-	      v[(j + 1) * n + k] = tmp[k] / hh;
+	      v [(j + 1) * n + k] = tmp [k] / hh;
 	    }
 	  /* rotate */
 	  for (i=0; i<j; i++)
 	    {
-	      r1 = h[ i      * m + j];
-	      r2 = h[(i + 1) * m + j];
-	      h[ i      * m + j] = c[i] * r1 - s[i] * r2;
-	      h[(i + 1) * m + j] = s[i] * r1 + c[i] * r2;
+	      r1 = h [ i      * m + j];
+	      r2 = h [(i + 1) * m + j];
+	      h [ i      * m + j] = c [i] * r1 - s [i] * r2;
+	      h [(i + 1) * m + j] = s [i] * r1 + c [i] * r2;
 	    }
-	  rr = h[j * m + j];
+	  rr = h [j * m + j];
 	  hv = sqrt (rr * rr + hh * hh); /* temporary variable */
-	  c[j] =  rr / hv;
-	  s[j] = -hh / hv;
-	  h[j * m + j] = hv; /* resultant (after rotated) element */
+	  c [j] =  rr / hv;
+	  s [j] = -hh / hv;
+	  h [j * m + j] = hv; /* resultant (after rotated) element */
 
-	  g0 = g[j];
-	  g[j    ] = c[j] * g0;
-	  g[j + 1] = s[j] * g0;
+	  g0 = g [j];
+	  g [j    ] = c [j] * g0;
+	  g [j + 1] = s [j] * g0;
 	}
       /* 3. form the approximate solution */
       /* solve y_k */
-      back_sub (m, m, h, g, c); /* use c[] as y_k */
+      back_sub (m, m, h, g, c); /* use c [] as y_k */
       /* x_m */
       for (i=0; i<n; i++)
 	{
 	  for (k=0; k<m; k++)
 	    {
-	      x[i] += v[k * n + i] * c[k];
+	      x [i] += v [k * n + i] * c [k];
 	    }
 	}
       /* 4. restart */
-      *err = fabs (g[m]); /* residual */
+      *err = fabs (g [m]); /* residual */
+      /*fprintf (stderr, "# iter %d res %e\n", *iter, *err);*/
       /* if satisfied, */
       if (*err <= tol) break;
       /* else */
@@ -127,15 +128,18 @@ mygmres_m (int n, double *f, double *x,
       myatimes (n, x, tmp);
       for (i=0; i<n; i++)
 	{
-	  tmp[i] = f[i] - tmp[i]; /* r_m */
+	  tmp [i] = f [i] - tmp [i]; /* r_m */
 	}
       /* compute v1 */
-      g[0] = norm (n, tmp);
+      g [0] = norm (n, tmp);
       for (i=0; i<n; i++)
 	{
-	  v[0 * n + i] = tmp[i] / g[0];
+	  v [0 * n + i] = tmp [i] / g [0];
 	}
     }
+
+  /* adjust iter */
+  (*iter) *= m;
 
   free (tmp);
   free (v);
@@ -180,24 +184,24 @@ mygmres (int n, double *f, double *x,
   myatimes (n, x, tmp);
   for (i=0; i<n; i++)
     {
-      tmp[i] = f[i] - tmp[i]; /* r0 */
+      tmp [i] = f [i] - tmp [i]; /* r0 */
     }
   /* compute v1 */
-  g[0] = norm (n, tmp); /* beta */
+  g [0] = norm (n, tmp); /* beta */
   for (i=0; i<n; i++)
     {
-      v[0 * n + i] = tmp[i] / g[0];
+      v [0 * n + i] = tmp [i] / g [0];
     }
   /* main loop */
   /* 2. iterate: */
   for (j=0; j<m; j++)
     {
       /* tmp = A.vj */
-      myatimes (n, &v[j * n], tmp);
+      myatimes (n, &v [j * n], tmp);
       /* h_i,j (i=1,...,j) */
       for (i=0; i<=j; i++)
 	{
-	  h[i * m + j] = inner (n, tmp, &v[i * n]);
+	  h [i * m + j] = inner (n, tmp, &v [i * n]);
 	}
       /* vv_j+1 */
       for (k=0; k<n; k++)
@@ -205,36 +209,36 @@ mygmres (int n, double *f, double *x,
 	  hv = 0.0;
 	  for (i=0; i<=j; i++)
 	    {
-	      hv += h[i * m + j] * v[i * n + k];
+	      hv += h [i * m + j] * v [i * n + k];
 	    }
-	  tmp[k] -= hv; /* vv_j+1 */
+	  tmp [k] -= hv; /* vv_j+1 */
 	}
       /* h_j+1,j */
       hh = norm (n, tmp); /* h_j+1,j */
       /* v_j+1 */
       for (k=0; k<n; k++)
 	{
-	  v[(j + 1) * n + k] = tmp[k] / hh;
+	  v [(j + 1) * n + k] = tmp [k] / hh;
 	}
       /* rotate */
       for (i=0; i<j; i++)
 	{
-	  r1 = h[ i      * m + j];
-	  r2 = h[(i + 1) * m + j];
-	  h[ i      * m + j] = c[i] * r1 - s[i] * r2;
-	  h[(i + 1) * m + j] = s[i] * r1 + c[i] * r2;
+	  r1 = h [ i      * m + j];
+	  r2 = h [(i + 1) * m + j];
+	  h [ i      * m + j] = c [i] * r1 - s [i] * r2;
+	  h [(i + 1) * m + j] = s [i] * r1 + c [i] * r2;
 	}
-      rr = h[j * m + j];
+      rr = h [j * m + j];
       hv = sqrt (rr * rr + hh * hh); /* temporary variable */
-      c[j] =  rr / hv;
-      s[j] = -hh / hv;
-      h[j * m + j] = hv; /* resultant (after rotated) element */
+      c [j] =  rr / hv;
+      s [j] = -hh / hv;
+      h [j * m + j] = hv; /* resultant (after rotated) element */
 
-      g0 = g[j];
-      g[j    ] = c[j] * g0;
-      g[j + 1] = s[j] * g0;
+      g0 = g [j];
+      g [j    ] = c [j] * g0;
+      g [j + 1] = s [j] * g0;
 
-      *err = fabs (g[j + 1]); /* residual */
+      *err = fabs (g [j + 1]); /* residual */
       /* if satisfied, */
       if (*err <= tol) break;
     }
@@ -242,13 +246,13 @@ mygmres (int n, double *f, double *x,
   *iter = j;
   /* 3. form the approximate solution */
   /* solve y_k */
-  back_sub (j, m, h, g, c); /* use c[] as y_k */
+  back_sub (j, m, h, g, c); /* use c [] as y_k */
   /* x_m */
   for (i=0; i<n; i++)
     {
       for (k=0; k<j; k++)
 	{
-	  x[i] += v[k * n + i] * c[k];
+	  x [i] += v [k * n + i] * c [k];
 	}
     }
 
@@ -264,7 +268,7 @@ void
 back_sub (int m, int nn,
 	  double *r, double *g, double *y)
 /* m  : number of iteration */
-/* nn : dimension of matrix r[] (nnxnn) */
+/* nn : dimension of matrix r [] (nnxnn) */
 {
   int i,j,jj;
 
@@ -273,13 +277,13 @@ back_sub (int m, int nn,
   for (jj=0; jj<m; jj++)
     {
       j = m - 1 - jj;
-      y[j] = 0.0;
+      y [j] = 0.0;
       for (i=j+1; i<m; i++)
 	{
-	  y[j] -= r[j * nn + i] * y[i];
+	  y [j] -= r [j * nn + i] * y [i];
 	}
-      y[j] += g[j];
-      y[j] = y[j] / r[j * nn + j];
+      y [j] += g [j];
+      y [j] = y [j] / r [j * nn + j];
     }
 }
 
@@ -291,7 +295,7 @@ norm (int n, double *x)
 
   ans = 0.0;
   for (i=0; i<n; i++)
-    ans += x[i] * x[i];
+    ans += x [i] * x [i];
   return sqrt (ans);
 }
 
@@ -303,6 +307,6 @@ inner (int n, double *x, double *y)
 
   ans = 0.0;
   for (i=0; i<n; i++)
-    ans += x[i] * y[i];
+    ans += x [i] * y [i];
   return ans;
 }
