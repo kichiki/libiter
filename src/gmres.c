4bbaacaeca543460c2c0dac1cb1428aebb6a6239
kengo ichiki <kengoichiki@gmail.com>
1999-09-05 22:33:18 +0000

    make use of BLAS routines in blas.c.

--- src/gmres.c
+++ src/gmres.c
@@ -1,6 +1,6 @@
 /* generalized minimum residual method
  * Copyright (C) 1998,1999 Kengo ICHIKI <kengo@caltech.edu>
- * $Id: gmres.c,v 1.8 1999/08/05 19:45:29 ichiki Exp $
+ * $Id: gmres.c,v 1.9 1999/09/05 22:33:18 ichiki Exp $
  *
  * Reference :
  *   GMRES(m) : Y.Saad & M.H.Schultz, SIAM J.Sci.Stat.Comput.
@@ -10,31 +10,27 @@
 #include <stdlib.h> /* malloc (), free() */
 #include <stdio.h>
 #include <math.h>
-#include "myroutines.h" /* my_d_malloc() */
+#include "../myroutines.h" /* my_d_malloc() */
+#include "blas.h"
 
 #include "mygmres.h"
 
 void
 mygmres_m (int n, double *f, double *x,
-	   int m,
-	   double tol,
-	   int itmax, int *iter, double *err,
+	   int m, double tol, int itmax,
+	   int *iter, double *err,
 	   void (*myatimes) (int, double *, double *))
 {
   /* solve linear system A.x = f */
   /* n: dimension of this sysmtem */
   /* m: # of iteration at once */
-  double norm(int n, double *x);
-  double inner(int n, double *x, double *y);
+  int i, j, k;
+  double hv;
+  double rr, hh;
+  double r1, r2;
+  double g0;
+  double *tmp, *v, *h, *g, *c, *s;
 
-  int
-    i,j,k;
-  double
-    hv,
-    rr,hh,
-    r1,r2,
-    g0,
-    *tmp, *v, *h, *g, *c, *s;
 
   tmp = my_d_malloc (n, "tmp");
   v   = my_d_malloc ((m + 1) * n, "v");
@@ -46,18 +42,13 @@ mygmres_m (int n, double *f, double *x,
   *iter = 0;
   /* 1. start: */
   /* compute r0 */
-  /* tmp = A.x0 */
   myatimes (n, x, tmp);
-  for(i=0; i<n; i++)
-    {
-      tmp [i] = f [i] - tmp [i]; /* r0 */
-    }
+  daxpyz (n, -1.0, tmp, 1, f, 1, tmp, 1);
   /* compute v1 */
-  g [0] = norm (n, tmp); /* beta */
-  for (i=0; i<n; i++)
-    {
-      v [0 * n + i] = tmp [i] / g [0];
-    }
+  /* beta */
+  g [0] = dnrm2 (n, tmp, 1);
+  dscalz (n, 1.0 / g [0], tmp, 1, & v [0], 1);
+
   /* main loop */
   while (*iter <= itmax)
     {
@@ -69,26 +60,19 @@ mygmres_m (int n, double *f, double *x,
 	  myatimes (n, &v [j * n], tmp);
 	  /* h_i,j (i=1,...,j) */
 	  for (i=0; i<=j; i++)
-	    {
-	      h [i * m + j] = inner (n, tmp, &v [i * n]);
-	    }
+	    h [i * m + j] = ddot (n, tmp, 1, & v [i * n], 1);
 	  /* vv_j+1 */
 	  for (k=0; k<n; k++)
 	    {
 	      hv = 0.0;
 	      for (i=0; i<=j; i++)
-		{
-		  hv += h [i * m + j] * v [i * n + k];
-		}
-	      tmp [k] -= hv; /* vv_j+1 */
+		hv += h [i * m + j] * v [i * n + k];
+	      tmp [k] -= hv;
 	    }
 	  /* h_j+1,j */
-	  hh = norm (n, tmp); /* h_j+1,j */
+	  hh = dnrm2 (n, tmp, 1);
 	  /* v_j+1 */
-	  for (k=0; k<n; k++)
-	    {
-	      v [(j + 1) * n + k] = tmp [k] / hh;
-	    }
+	  dscalz (n, 1.0 / hh, tmp, 1, & v [(j + 1) * n], 1);
 	  /* rotate */
 	  for (i=0; i<j; i++)
 	    {
@@ -112,12 +96,9 @@ mygmres_m (int n, double *f, double *x,
       back_sub (m, m, h, g, c); /* use c [] as y_k */
       /* x_m */
       for (i=0; i<n; i++)
-	{
-	  for (k=0; k<m; k++)
-	    {
-	      x [i] += v [k * n + i] * c [k];
-	    }
-	}
+	for (k=0; k<m; k++)
+	  x [i] += v [k * n + i] * c [k];
+
       /* 4. restart */
       *err = fabs (g [m]); /* residual */
       /*fprintf (stderr, "# iter %d res %e\n", *iter, *err);*/
@@ -127,16 +108,11 @@ mygmres_m (int n, double *f, double *x,
       /* compute r_m */
       /* tmp = A.x_m */
       myatimes (n, x, tmp);
-      for (i=0; i<n; i++)
-	{
-	  tmp [i] = f [i] - tmp [i]; /* r_m */
-	}
+      /* r_m */
+      daxpyz (n, -1.0, tmp, 1, f, 1, tmp, 1);
       /* compute v1 */
-      g [0] = norm (n, tmp);
-      for (i=0; i<n; i++)
-	{
-	  v [0 * n + i] = tmp [i] / g [0];
-	}
+      g [0] = dnrm2 (n, tmp, 1);
+      dscalz (n, 1.0 / g [0], tmp, 1, & v [0], 1);
     }
 
   /* adjust iter */
@@ -152,23 +128,19 @@ mygmres_m (int n, double *f, double *x,
 
 void
 mygmres (int n, double *f, double *x,
-	 double tol,
-	 int itmax, int *iter, double *err,
+	 double tol, int itmax,
+	 int *iter, double *err,
 	 void (*myatimes) (int, double *, double *))
 {
   /* solve linear system A.x = f */
   /* n: dimension of this sysmtem */
-  double norm(int n, double *x);
-  double inner(int n, double *x, double *y);
+  int i, j, k, m;
+  double hv;
+  double rr, hh;
+  double r1, r2;
+  double g0;
+  double *tmp, *v, *h, *g, *c, *s;
 
-  int
-    i,j,k,m;
-  double
-    hv,
-    rr,hh,
-    r1,r2,
-    g0,
-    *tmp, *v, *h, *g, *c, *s;
 
   m = itmax;
 
@@ -179,20 +151,15 @@ mygmres (int n, double *f, double *x,
   c   = my_d_malloc (m, "c");
   s   = my_d_malloc (m, "s");
 
+
   /* 1. start: */
   /* compute r0 */
-  /* tmp = A.x0 */
   myatimes (n, x, tmp);
-  for (i=0; i<n; i++)
-    {
-      tmp [i] = f [i] - tmp [i]; /* r0 */
-    }
+  daxpyz (n, -1.0, tmp, 1, f, 1, tmp, 1);
   /* compute v1 */
-  g [0] = norm (n, tmp); /* beta */
-  for (i=0; i<n; i++)
-    {
-      v [0 * n + i] = tmp [i] / g [0];
-    }
+  g [0] = dnrm2 (n, tmp, 1); /* beta */
+  dscalz (n, 1.0 / g [0], tmp, 1, & v [0], 1);
+
   /* main loop */
   /* 2. iterate: */
   for (j=0; j<m; j++)
@@ -201,26 +168,19 @@ mygmres (int n, double *f, double *x,
       myatimes (n, &v [j * n], tmp);
       /* h_i,j (i=1,...,j) */
       for (i=0; i<=j; i++)
-	{
-	  h [i * m + j] = inner (n, tmp, &v [i * n]);
-	}
+	h [i * m + j] = ddot (n, tmp, 1, &v [i * n], 1);
       /* vv_j+1 */
       for (k=0; k<n; k++)
 	{
 	  hv = 0.0;
 	  for (i=0; i<=j; i++)
-	    {
-	      hv += h [i * m + j] * v [i * n + k];
-	    }
-	  tmp [k] -= hv; /* vv_j+1 */
+	    hv += h [i * m + j] * v [i * n + k];
+	  tmp [k] -= hv;
 	}
       /* h_j+1,j */
-      hh = norm (n, tmp); /* h_j+1,j */
+      hh = dnrm2 (n, tmp, 1);
       /* v_j+1 */
-      for (k=0; k<n; k++)
-	{
-	  v [(j + 1) * n + k] = tmp [k] / hh;
-	}
+      dscalz (n, 1.0 / hh, tmp, 1, & v [(j + 1) * n], 1);
       /* rotate */
       for (i=0; i<j; i++)
 	{
@@ -245,17 +205,14 @@ mygmres (int n, double *f, double *x,
     }
   j++;/* this is because ++(*iter) in gmres(m) */
   *iter = j;
+
   /* 3. form the approximate solution */
   /* solve y_k */
   back_sub (j, m, h, g, c); /* use c [] as y_k */
   /* x_m */
   for (i=0; i<n; i++)
-    {
-      for (k=0; k<j; k++)
-	{
-	  x [i] += v [k * n + i] * c [k];
-	}
-    }
+    for (k=0; k<j; k++)
+      x [i] += v [k * n + i] * c [k];
 
   free (tmp);
   free (v);
@@ -271,7 +228,7 @@ back_sub (int m, int nn,
 /* m  : number of iteration */
 /* nn : dimension of matrix r [] (nnxnn) */
 {
-  int i,j,jj;
+  int i, j, jj;
 
   /*for (j=m-1;j>=0;j--){*/
   /* above for-loop fail, because j is unsigned!! */
@@ -279,35 +236,11 @@ back_sub (int m, int nn,
     {
       j = m - 1 - jj;
       y [j] = 0.0;
-      for (i=j+1; i<m; i++)
+      for (i= j + 1; i<m; i++)
 	{
 	  y [j] -= r [j * nn + i] * y [i];
 	}
       y [j] += g [j];
       y [j] = y [j] / r [j * nn + j];
     }
-}
-
-double
-norm (int n, double *x)
-{
-  int i;
-  double ans;
-
-  ans = 0.0;
-  for (i=0; i<n; i++)
-    ans += x [i] * x [i];
-  return sqrt (ans);
-}
-
-double
-inner (int n, double *x, double *y)
-{
-  int i;
-  double ans;
-
-  ans = 0.0;
-  for (i=0; i<n; i++)
-    ans += x [i] * y [i];
-  return ans;
 }
