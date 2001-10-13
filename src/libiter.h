6c11ad90c6c3cd5f08627cce537fe10d520aa4be
kengo ichiki <kengoichiki@gmail.com>
2001-10-13 12:17:38 +0000

    revised for Rev 2 series.

--- src/libiter.h
+++ src/libiter.h
@@ -1,6 +1,6 @@
 /* header file for library 'iter' -- mygmres.c, bi-cgstab.c, and orthomin.c.
- * Copyright (C) 2001 Kengo Ichiki <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: libiter.h,v 1.8 2001/02/09 06:55:22 ichiki Exp $
+ * Copyright (C) 2001 Kengo Ichiki <ichiki@haloumi.tn.utwente.nl>
+ * $Id: libiter.h,v 2.1 2001/10/13 12:17:38 ichiki Exp $
  */
 
 /* from blas.h */
@@ -25,76 +25,101 @@ dscalz (int n, double da, double *dx, int incx,
 	double *dz, int incz);
 
 /* from mygmres.h */
-extern int GMRES_it_max;
-extern int GMRES_it_restart;
-extern double GMRES_eps;
+/* header file of mygmres.c
+ * Copyright (C) 1998-2001 Kengo Ichiki <ichiki@haloumi.tn.utwente.nl>
+ * Id: mygmres.h,v 2.1 2001/10/13 12:12:18 ichiki Exp
+ */
 
 void
 solve_iter_gmres (int n,
 		  double *b, double *x,
-		  void (*atimes) (int, double *, double *));
+		  void (*atimes) (int, double *, double *, void *),
+		  void * user_data,
+		  int it_max, int it_restart, double eps);
 void
 mygmres_m (int n, double *f, double *x,
 	   int m, double tol, int itmax,
 	   int *iter, double *err,
-	   void (*myatimes) (int, double *, double *));
+	   void (*myatimes) (int, double *, double *, void *),
+	   void * user_data);
 void
 mygmres (int n, double *f, double *x,
 	 double tol, int itmax,
 	 int *iter, double *err,
-	 void (*myatimes) (int, double *, double *));
+	 void (*myatimes) (int, double *, double *, void *),
+	 void * user_data);
 
 /* from bi-cgstab.h */
-/** global variables **/
-extern int STAB_it_max;
-extern double STAB_log10_eps;
-
+/* header file of bi-cgstab.c (wrapper for iterative solver routines)
+ * Copyright (C) 1999-2001 Kengo Ichiki <ichiki@haloumi.tn.utwente.nl>
+ * Id: bi-cgstab.h,v 2.1 2001/10/13 11:46:15 ichiki Exp
+ */
 
 void
 solve_iter_stab (int n,
 		 double *b, double *x,
-		 void (*atimes) (int, double *, double *),
+		 void (*atimes) (int, double *, double *, void *),
+		 void * user_data,
 		 void (*solver) (int, double *, double *, int,
 				 double, double, int *, double *,
-				 void (*) (int, double *, double *)));
-
+				 void (*)
+				 (int, double *, double *, void *),
+				 void *),
+		 int it_max, double log10_eps);
 void
 sta (int m, double *b, double *x, int kend,
      double eps, double hnor,
      int *iter, double *hg,
-     void (*myatimes) (int, double *, double *));
+     void (*myatimes) (int, double *, double *, void *),
+     void * user_data);
 void
-st2 (int m, double *b, double *x, int kend,
-     double eps, double hnor,
-     int *iter, double *hg,
-     void (*myatimes) (int, double *, double *));
+sta2 (int m, double *b, double *x, int kend,
+      double eps, double hnor,
+      int *iter, double *hg,
+      void (*myatimes) (int, double *, double *, void *),
+      void * user_data);
 void
 st2_chk (int m, double *b, double *x, int kend,
 	 double eps, double hnor,
 	 int *iter, double *hg,
-	 void (*myatimes) (int, double *, double *));
+	 void (*myatimes) (int, double *, double *, void *),
+	 void * user_data);
 void
 gpb (int m, double *b, double *x, int kend,
      double eps, double hnor,
      int *iter, double *hg,
-     void (*myatimes) (int, double *, double *));
+     void (*myatimes) (int, double *, double *, void *),
+     void * user_data);
 void
 gpb_chk (int m, double *b, double *x, int kend,
 	 double eps, double hnor,
 	 int *iter, double *hg,
-	 void (*myatimes) (int, double *, double *));
+	 void (*myatimes) (int, double *, double *, void *),
+	 void * user_data);
 
 /* from orthomin.h */
+/* header file of orthomin.c
+ * Copyright (C) 1999-2001 Kengo Ichiki <ichiki@haloumi.tn.utwente.nl>
+ * Id: orthomin.h,v 2.1 2001/10/13 12:01:56 ichiki Exp
+ */
+
 void
 solve_iter_otmk (int n,
 		 double *b, double *x,
-		 void (*atimes) (int, double *, double *),
-		 void (*solver) (int, double *, double *, int, int,
-				 double, double, int *, double *,
-				 void (*) (int, double *, double *)));
+		 void (*atimes) (int, double *, double *, void *),
+		 void * user_data,
+		 void (*solver) (int, double *, double *,
+				 int, int, double,
+				 double, int *, double *,
+				 void (*)
+				 (int, double *, double *, void *),
+				 void *),
+		 int it_max, double log10_eps,
+		 int it_restart);
 void
 otmk (int m, double *b, double *x,
       int kres, int kend,
       double eps, double hnor,
       int *iter, double *hg,
-      void (*myatimes) (int, double *, double *));
+      void (*myatimes) (int, double *, double *, void *),
+      void * user_data);
