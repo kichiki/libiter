8bab817f5f0670b8341d1be96bc3a3bb3bf73fe8
kengo ichiki <kengoichiki@gmail.com>
2006-09-26 16:48:57 +0000

    file name is changed from 'blas.c' to 'myblas.c'. put the GPL notice.

--- src/myblas.c
+++ src/myblas.c
@@ -1,12 +1,24 @@
-/* BLAS
+/* Excerpted from BLAS package
  * Copyright (C) 1999-2006 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: myblas.c,v 1.3 2006/09/26 05:16:17 ichiki Exp $
+ * $Id: myblas.c,v 1.4 2006/09/26 16:48:57 ichiki Exp $
  *
- * Excerpted from : BLAS package
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 #include <math.h>
 
-#include "blas.h"
+#include "myblas.h"
 
 /*    constant times a vector plus a vector.
  *    uses unrolled loops for increments equal to one.
