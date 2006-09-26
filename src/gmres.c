f5688470d95ed6fb9ea75cce3d62c900f589792a
kengo ichiki <kengoichiki@gmail.com>
2006-09-26 16:51:42 +0000

    file name is changed from 'mygmres.c' to 'gmres.c'. put the GPL notice.

--- src/gmres.c
+++ src/gmres.c
@@ -1,24 +1,37 @@
 /* generalized minimum residual method
- * Copyright (C) 1998-2003 Kengo Ichiki <ichiki@jhu.edu>
- * $Id: gmres.c,v 2.5 2006/09/26 05:18:55 ichiki Exp $
+ * Copyright (C) 1998-2006 Kengo Ichiki <kichiki@users.sourceforge.net>
+ * $Id: gmres.c,v 2.6 2006/09/26 16:51:42 ichiki Exp $
  *
  * Reference :
  *   GMRES(m) : Y.Saad & M.H.Schultz, SIAM J.Sci.Stat.Comput.
  *   vol7 (1986) pp.856-869
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 #include <stdlib.h> /* malloc (), free() */
 #include <stdio.h>
 #include <math.h>
-#include "blas.h"
+#include "myblas.h"
 
-#include "mygmres.h"
+#include "gmres.h"
 
 /** global variables **/
-int ITER_mygmres_debug; /* [0|1]: [not print/print] iter and res */
+int ITER_gmres_debug; /* [0|1]: [not print/print] iter and res */
 
 
-/* wrapper routine for mygmres_m ()
+/* wrapper routine for gmres_m ()
  * INPUT
  *   n : size of vectors v[] and f[] -- expected to be np * nelm for red-sym
  *   b [n] : given vector
@@ -36,18 +49,18 @@ solve_iter_gmres (int n,
 		  void * user_data,
 		  int it_max, int it_restart, double eps)
 {
-  extern int ITER_mygmres_debug; /* [0|1]: [not print/print] iter and res */
+  extern int ITER_gmres_debug; /* [0|1]: [not print/print] iter and res */
   double residual;
   int iter;
 
 
-  mygmres_m (n, b, x,
-	     it_restart, eps, it_max,
-	     &iter, &residual,
-	     atimes,
-	     user_data);
+  gmres_m (n, b, x,
+	   it_restart, eps, it_max,
+	   &iter, &residual,
+	   atimes,
+	   user_data);
 
-  if (ITER_mygmres_debug)
+  if (ITER_gmres_debug)
     fprintf (stderr, "# iter=%d res=%e\n", iter, residual);
 }
 
@@ -73,11 +86,11 @@ back_sub (int m, int nn,
     }
 }
 void
-mygmres_m (int n, const double *f, double *x,
-	   int m, double tol, int itmax,
-	   int *iter, double *res,
-	   void (*myatimes) (int, const double *, double *, void *),
-	   void * user_data)
+gmres_m (int n, const double *f, double *x,
+	 int m, double tol, int itmax,
+	 int *iter, double *res,
+	 void (*myatimes) (int, const double *, double *, void *),
+	 void * user_data)
 {
   /* solve linear system A.x = f */
   /* n: dimension of this sysmtem */
@@ -192,11 +205,11 @@ mygmres_m (int n, const double *f, double *x,
 }
 
 void
-mygmres (int n, const double *f, double *x,
-	 double tol, int itmax,
-	 int *iter, double *res,
-	 void (*myatimes) (int, const double *, double *, void *),
-	 void * user_data)
+gmres (int n, const double *f, double *x,
+       double tol, int itmax,
+       int *iter, double *res,
+       void (*myatimes) (int, const double *, double *, void *),
+       void * user_data)
 {
   /* solve linear system A.x = f */
   /* n: dimension of this sysmtem */
