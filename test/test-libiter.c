3604d23253c89dec642705cb1b5c0e98c586d590
kengo ichiki <kengoichiki@gmail.com>
2007-12-01 18:12:38 +0000

    add check for preconditioned routines.

--- test/test-libiter.c
+++ test/test-libiter.c
@@ -1,6 +1,6 @@
 /* test code for libiter solvers
  * Copyright (C) 2006-2007 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: test-libiter.c,v 1.4 2007/11/25 19:11:55 kichiki Exp $
+ * $Id: test-libiter.c,v 1.5 2007/12/01 18:12:38 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,6 +23,12 @@
 #include "check-iter-gen.h"
 #include "toeplitz.h"
 
+#include "check-cg-pc.h"
+#include "check-gmres-m-pc.h"
+#include "check-otmk-pc.h"
+#include "check-sta-pc.h"
+#include "check-sta2-pc.h"
+#include "check-gpb-pc.h"
 
 /* main program */
 int
@@ -43,6 +49,14 @@ main (int argc, char** argv)
   check += Toeplitz_check_all (1000, 1.5, // n, gamma
 			       10000, 20, 1.0e-7, // max, restart, eps
 			       1, 3.5e-6); // for check
+
+  int n = 2000;
+  check += check_cg_pc      (n, 1000,     1.0e-12, 1, 3.0e-10);
+  check += check_gmres_m_pc (n, 1000, 20, 1.0e-12, 1, 7.0e-10);
+  check += check_otmk_pc    (n, 1000, 20, 1.0e-12, 1, 5.5e-9);
+  check += check_sta_pc     (n, 1000,     1.0e-12, 1, 2.9e-9);
+  check += check_sta2_pc    (n, 1000,     1.0e-12, 1, 4.6e-9);
+  check += check_gpb_pc     (n, 1000,     1.0e-12, 1, 7.8e-9);
 
   fprintf (stdout,
 	   "==================================================\n"
